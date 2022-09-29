ce7ac848d2eb0d73c40361ea9948d2651ac6b8bf
Karan Baweja <karanbaweja@Karan-Bawejas-MacBook-Pro.local>
2022-09-29 17:37:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000
+
+//connect to mongoDB atlas 
+mongoose.connect(
+    process.env.MONGO_URL,
+    {useNewUrlParser:true}
+    )
+    .then(() => {
+        console.log("Connected to MongoDB Atlas");
+}).catch(error => {
+    console.log("Something wrong happened",error);
+});
+
+// start server
+app.listen(PORT,() => {
+    console.log("Server started at PORT", PORT);
+})
